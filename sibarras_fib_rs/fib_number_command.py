3c9a381ec23d55093c799da43192e4a0f5709a7f
Samuel Ibarra <ing.samuelibarra@gmail.com>
2022-08-01 01:52:45 -0500

    adding function

---
+++ sibarras_fib_rs/fib_number_command.py
@@ -0,0 +1,12 @@
+import argparse
+
+from .sibarras_fib_rs import fibonacci_number
+
+def fib_number_command() -> None:
+    parser = argparse.ArgumentParser(description='Calculate Fibonacci numbers')
+    parser.add_argument('--number', action='store',
+        type=int, required=True,
+        help="Fibonacci number to becalculated"
+    )
+    args = parser.parse_args()
+    print(f"Your Fibonacci number is: {fibonacci_number(n=args.number)}")
