e26c531cfcf43168edff33432d6880161bd1ea56
Samuel Ibarra <ing.samuelibarra@gmail.com>
2022-08-01 01:26:59 -0500

    adding new functiosn

---
+++ src/fib_calcs/fib_numbers.rs
@@ -0,0 +1,11 @@
+use pyo3::prelude::pyfunction;
+use super::fib_number::fibonacci_number;
+
+#[pyfunction]
+pub fn fibonacci_numbers(numbers: Vec<i32>) -> Vec<u64> {
+    let mut vec: vec![];
+    for n in numbers {
+        vec.push(fibonacci_number(n));
+    }
+    return vec;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+mod fib_calcs;
+use fib_calcs::{
+    fib_number::fibonacci_number,
+    fib_numbers::fibonacci_numbers
+};
 
 #[pyfunction]
 fn say_hello() {
@@ -9,5 +14,7 @@ fn say_hello() {
 #[pymodule]
 fn sibarras_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(say_hello))?;
+    m.add_wrapped(wrap_pyfunction!(fibonacci_number))?;
+    m.add_wrapped(wrap_pyfunction!(fibonacci_numbers))?;
     Ok(())
 }
